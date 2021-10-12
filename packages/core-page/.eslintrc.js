5f34676de5bb293f864b5fabdbc9a1dec8d83d6a
wzb <948079797@qq.com>
2021-10-12 11:38:42 +0800

    添加权限规则配置，添加各式依赖

---
+++ packages/core-page/.eslintrc.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-template': 'error',
+    'no-extra-semi': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-extra-semi': ['error'],
+    'linebreak-style': ['error', 'unix'],
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': 1,
+    'prettier/prettier': 'error',
+  },
+}
